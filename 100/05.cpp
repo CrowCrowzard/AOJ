b1b758a0f4fed9c15055b85eb07056dd682c3914
Yuta_Muraki <crowizard@gmail.com>
2014-09-10 22:45:06 +0900

    10000's file push

---
+++ 100/05.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  for(int i = 0; i < 1000; i++) {
+    cout << "Hello World" << endl;
+  }
+}
+
+/*
+int i = 0;
+for(i = 0; i < 1000; i++) {
+	printf("Hello World\n");
+}
+
+while(i < 1000) {
+	printf("Hello World\n");
+	i++;
+}
+*/
